a07356317143496e822982cadad62920d48e6ccb
balo-max <shoni6124@gmail.com>
2025-02-20 19:55:41 +0100

    fix task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import Profile from "./Profile/Profile"
+import userData from "../userData.json"
 
 export default function App () {
   return (
     <>
-      <Profile />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats} />
     </>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,18 @@
-import userData from "../../userData.json"
 import ProfileImgText from "../ProfileImgText/ProfileImgText";
 import ProfileList from "../ProfileList/ProfileList";
 import css from "./Profile.module.css"
 
-export default function Profile () {
+export default function Profile (props) {
     return (
         <div className={css.container}>
-            <div>
-                <ProfileImgText
-                    name={userData.username}
-                    tag={userData.tag}
-                    location={userData.location}
-                    image={userData.avatar} />
-            </div>
+            <ProfileImgText
+                name={props.username}
+                tag={props.tag}
+                location={props.location}
+                image={props.avatar} />
+
             <ProfileList
-                stats={userData.stats } />
+                stats={props.stats} />
         </div>
     );
 }
