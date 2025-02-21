df659194b05f65019bcd10bd2fc30bb3a6b532aa
balo-max <shoni6124@gmail.com>
2025-02-21 19:27:59 +0100

    fix profile tag

--- src/components/ProfileImgText/ProfileImgText.jsx
+++ src/components/ProfileImgText/ProfileImgText.jsx
@@ -8,7 +8,7 @@ export default function ProfileImgText({ name, tag, location, image }) {
                 alt={name}
             />
             <p className={css.name}>{name}</p>
-            <p className={css.tag}>{tag}</p>
+            <p className={css.tag}>@{tag}</p>
             <p className={css.location}>{location}</p>
         </div>
     );
